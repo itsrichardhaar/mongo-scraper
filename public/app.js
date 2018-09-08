2c9d399fd1add9adbfb2fcd85567a9062aeab6c7
Richard Haar <itsrichardhaar@MacBook-Pro-3.lan1>
2018-09-08 09:07:00 -0400

    commit

--- public/app.js
+++ public/app.js
@@ -1,12 +1,11 @@
 
 $.getJSON("/articles", function(data) {
-    // For each one
+   
     for (var i = 0; i < data.length; i++) {
       
       $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
     }
   });
-  
   
   
   $(document).on("click", "p", function() {
--- public/server.js
+++ public/server.js
@@ -2,15 +2,13 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-var axios = require("axios");
 var cheerio = require("cheerio");
+var request = require("request");
 
 
 var db = require("./models");
 
 var PORT = 3000;
-
-
 var app = express();
 
 
@@ -34,37 +32,38 @@ db.once('open', function () {
 // Routes
 
 
-app.get("/scrape", function (req, res) {
-
-    axios.get("http://www.echojs.com/").then(function (response) {
-
-        var $ = cheerio.load(response.data);
-
-        $("article h2").each(function (i, element) {
-
-            var result = {};
-
-            result.title = $(this)
-                .children("a")
-                .text();
-            result.link = $(this)
-                .children("a")
-                .attr("href");
-
-
-            db.Article.create(result)
-                .then(function (dbArticle) {
-                    e
-                    console.log(dbArticle);
-                })
-                .catch(function (err) {
-
-                    return res.json(err);
-                });
-        });
-
-        res.send("Scrape Complete");
-    });
+app.get("/scrape", function(req, res) {
+	request("https://www.nytimes.com/section/world", function(error, response, html) {
+		var $ = cheerio.load(html);
+		var result = {};
+		$("div.story-body").each(function(i, element) {
+			var link = $(element).find("a").attr("href");
+			var title = $(element).find("h2.headline").text().trim();
+			var summary = $(element).find("p.summary").text().trim();
+			var img = $(element).parent().find("figure.media").find("img").attr("src");
+			result.link = link;
+			result.title = title;
+			if (summary) {
+				result.summary = summary;
+			};
+			if (img) {
+				result.img = img;
+			}
+			else {
+				result.img = $(element).find(".wide-thumb").find("img").attr("src");
+			};
+			var entry = new Article(result);
+			Article.find({title: result.title}, function(err, data) {
+				if (data.length === 0) {
+					entry.save(function(err, data) {
+						if (err) throw err;
+					});
+				}
+			});
+		});
+		console.log("Scrape finished.");
+		res.redirect("/");
+	});
 });
 
 app.get("/articles", function (req, res) {
