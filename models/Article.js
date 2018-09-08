28b9bc7c83a84270d282f379f69f7ea8c9753690
Richard Haar <itsrichardhaar@MacBook-Pro-3.lan1>
2018-09-07 23:59:46 -0400

    commit

--- models/Article.js
+++ models/Article.js
@@ -3,19 +3,17 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var ArticleSchema = new Schema({
-    // `title` is required and of type String
+   
     title: {
       type: String,
       required: true
     },
-    // `link` is required and of type String
+    
     link: {
       type: String,
       required: true
     },
-    // `note` is an object that stores a Note id
-    // The ref property links the ObjectId to the Note model
-    // This allows us to populate the Article with an associated Note
+ 
     note: {
       type: Schema.Types.ObjectId,
       ref: "Note"
@@ -24,5 +22,4 @@ var ArticleSchema = new Schema({
 
   var Article = mongoose.model("Article", ArticleSchema);
 
-// Export the Article model
 module.exports = Article;
